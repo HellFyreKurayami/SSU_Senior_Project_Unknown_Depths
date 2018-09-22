6bcde1ad3a0c5f03aa9ee5384f0e308d5a2c1cc0
TravisJEngle <TJEngle97@gmail.com>
2018-09-22 13:04:55 -0400

    Camera Movement
    Currently attactched to mouse positioning, will be attached to a character later

---
+++ Unknown Depths/Assets/Scripts/PixPerCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PixPerCam Class
+
+//Most of this code is in refernece to a 2D Map
+//Generation tutuorial found on LinkedIn Learning or
+//Lynda.com. Tutorial by Jesse Freeman
+
+//https://www.linkedin.com/learning/unity-5-2d-random-map-generation
+
+public class PixPerCam : MonoBehaviour {
+
+
+    public static float pixToUnit = 1f;
+    public static float scale = 1;
+
+    public Vector2 nativeRes = new Vector2(160, 144);
+
+    private void Awake()
+    {
+        var camera = GetComponent<Camera>();
+
+        if (camera.orthographic)
+        {
+            var dir = Screen.height;
+            var res = nativeRes.y;
+
+            scale = dir / res;
+            pixToUnit *= scale;
+
+            camera.orthographicSize = (dir / 2.0f) / pixToUnit;
+        }
+    }
+}
